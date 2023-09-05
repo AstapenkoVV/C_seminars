7ff6256ae6b8827a7900e3d490736a2adb805bd0
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-05 17:58:02 +0300

    Create Seminar_1 homework_2

---
+++ Seminar_1/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдаёт, какое число большее, 
+// а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+string num1 = (Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+string num2 = (Console.ReadLine());
+bool result1 = int.TryParse(num1, out int number1);
+bool result2 = int.TryParse(num2, out int number2);
+if (result1 == true && result2 == true)
+    {
+        if (number1 > number2)
+        Console.WriteLine("Первое число больше второго!");
+        else Console.WriteLine("Второе число больше первого!");
+    }
+else Console.WriteLine("Введенные вами данные не являются числами, сравнение невозможно!!!");
