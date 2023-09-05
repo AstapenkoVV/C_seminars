4156ff7a54976e712f9cd74111dfcfb642eeefa5
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-05 18:50:32 +0300

    Create Seminar_1 homework_6

---
+++ Seminar_1/task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число 
+// чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите первое число: ");
+string num = (Console.ReadLine());
+bool result = int.TryParse(num, out int number);
+if (result == true)
+    {
+        if (number%2 == 0)
+        Console.WriteLine("Число " + number + " делится на два без остатка");
+        else Console.WriteLine("Число " + number + " НЕ делится на два без остатка");    }
+else Console.WriteLine("Введенные вами данные не являются числами, сравнение невозможно!!!");
