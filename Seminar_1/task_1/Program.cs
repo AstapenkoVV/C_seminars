7f6b170ac595d20e2280dd02649a9629bee464f2
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-05 18:03:10 +0300

    Editind Seminar_1 homework_2

--- Seminar_1/task_1/Program.cs
+++ Seminar_1/task_1/Program.cs
@@ -11,7 +11,7 @@ bool result2 = int.TryParse(num2, out int number2);
 if (result1 == true && result2 == true)
     {
         if (number1 > number2)
-        Console.WriteLine("Первое число больше второго!");
-        else Console.WriteLine("Второе число больше первого!");
+        Console.WriteLine("Первое число больше второго! Max = " + number1);
+        else Console.WriteLine("Второе число больше первого! Max = " + number2);
     }
 else Console.WriteLine("Введенные вами данные не являются числами, сравнение невозможно!!!");
