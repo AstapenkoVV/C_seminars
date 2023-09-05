354fce30b090d7648f0e7cd047ac91f6c3f6e065
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-05 19:11:36 +0300

    Create Seminar_1 homework_8

---
+++ Seminar_1/task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string num = (Console.ReadLine());
+bool result = int.TryParse(num, out int number);
+if (result == true)
+    {
+        for (int i = 2; i <= number; i++)
+        {
+            if ((i % 2 == 0 && i == number) || (i % 2 == 0 && i + 1 == number))
+            Console.Write(" " + i);
+            else if (i % 2 == 0)
+            Console.Write(" " + i + ",");
+        }
+    }
+else Console.WriteLine("Введенные вами данные не являются числами, проверка на четность невозможна!!!");
