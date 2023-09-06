6b85e0c887f21a5e05383d44bd54832604eeb236
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-06 20:27:46 +0300

    Create Seminar_2 homework_10

---
+++ Seminar_2/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+Console.WriteLine("Введите 3х значное число: ");
+string num = (Console.ReadLine());
+// Console.WriteLine(num.Length);
+if (num.Length == 3)
+{   bool result = int.TryParse(num, out int number);
+    if (result == true)
+        {
+            int n = number / 10;
+            n = n % 10;
+            Console.WriteLine("Втарая цифра числа: " + n);
+        }
+    else Console.WriteLine("Введенные вами данные не являются числом!!!");
+}
+else Console.WriteLine("Введенные вами данные не являются числом или это не 3х значное число.");
