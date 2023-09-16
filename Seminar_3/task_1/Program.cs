75c6d85ce4eb2d1f97948786fb11260d788b22b5
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-16 08:20:25 +0300

    Create Seminar_3 homework_1

---
+++ Seminar_3/task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+        if ((number > 9999) && (number < 100000))
+        {
+          int a = number/10000;
+          int b = number%10000/1000;
+          int c = number%100/10;
+          int d = number%10;
+          if ((a == d) && (b == c))
+          {
+            return true;
+          }
+                else 
+                {
+                  return true;
+                }
+        }
+      else 
+      {
+        System.Console.WriteLine("Число не пятизначное");
+        return false;
+      }
+}
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
