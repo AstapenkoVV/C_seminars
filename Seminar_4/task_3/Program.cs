d43de96845a8cb078c2937ac71768c2d4b7589d6
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-21 20:40:46 +0300

    Create Seminar_4 homework_29

---
+++ Seminar_4/task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит 
+// их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] ArrayNum(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++) 
+    {
+        array[i] = random.Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+}
+
+int length = WorkWithUser("Введите размер массива: ");
+int min = WorkWithUser("Введите начальный диапазон массива: ");
+int max = WorkWithUser("Введите конечный диапазон массива: ");
+int[] array = ArrayNum(length, min, max);
+PrintArray(array);
