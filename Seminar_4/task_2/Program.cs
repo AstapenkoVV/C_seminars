ef7aabe45f4d35cd15d6877eb5dd79efe8ff2e20
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-21 20:40:29 +0300

    Create Seminar_4 homework_27

---
+++ Seminar_4/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SumNumbers(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int n = WorkWithUser("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе = {SumNumbers(n)}");
