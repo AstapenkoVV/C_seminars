60bf9f9a32283211550ddcc7bf98f41d050fe2be
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-21 20:40:13 +0300

    Create Seminar_4 homework_25

---
+++ Seminar_4/task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int Work(int number, int exponent)
+{
+    int temp = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        temp *= number;
+    }
+    return temp;
+}
+
+int a = WorkWithUser("Введите число А: ");
+int b = WorkWithUser("Введите степень В: ");
+if (b > 0) Console.WriteLine($"Число {a} в сепени {b} равно {Work(a, b)}");
+else Console.WriteLine("Степень не натуральная!");
