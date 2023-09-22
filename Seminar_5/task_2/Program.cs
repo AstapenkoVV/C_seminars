728e58a604ccdef8398ce1d2f3b220975dfce77c
Vladimir Astapenko <wccvlast@mail.ru>
2023-09-22 16:23:40 +0300

    Create Seminar_5 homework_36

---
+++ Seminar_5/task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[] ArrayNum(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void CounterNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum = sum + array[i];
+    }
+    Console.WriteLine($"Массив: {string.Join<int>(", ", array)}");
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в массиве: {sum}");
+}
+
+int length = WorkWithUser("Введите размер массива: ");
+int min = WorkWithUser("Введите минимальный элемент массива: ");
+int max = WorkWithUser("Введите максимальный элемент массива: ");
+int[] array = ArrayNum(length, min, max);
+CounterNumbers(array);
