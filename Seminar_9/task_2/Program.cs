416c7a7a33f84d6b3ac2b70f0d9e295af43666fe
Vladimir Astapenko <wccvlast@mail.ru>
2023-10-05 21:17:26 +0300

    Create Seminar_9 homework_66

---
+++ Seminar_9/task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в 
+промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SummNumbers(int m, int n)
+{
+    if (m > n) return 0;
+    else return m + SummNumbers(m + 1, n);
+}
+
+int m = WorkWithUser("Введите наименьшее число M: ");
+int n = WorkWithUser("Введите наибольшее число N: ");
+Console.WriteLine(SummNumbers(m, n));
