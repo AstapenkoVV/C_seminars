d00c69001c8d869d423fe91706f4762002cbd52f
Vladimir Astapenko <wccvlast@mail.ru>
2023-10-05 21:17:03 +0300

    Create Seminar_9 homework_64

---
+++ Seminar_9/task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в 
+промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int WorkWithUser(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string ShowNumbers(int a, int number)
+{
+    if (a <= number) return ShowNumbers(a + 1, number) + $"{a} ";
+    else return String.Empty;
+}
+int a = 1;
+int n = WorkWithUser("Введите число N: ");
+Console.WriteLine($"[ {ShowNumbers(a, n)}]");
